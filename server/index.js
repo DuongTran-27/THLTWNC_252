5518700c05b95a0d996c08fabaa1c53bb10b1abc
Duong Tran <duongtran@example.com>
2026-04-02 18:39:49 +0700

    pushcode

--- server/index.js
+++ server/index.js
@@ -25,6 +25,19 @@ app.use('/api/admin', require('./api/admin.js'));
 // customer routes
 app.use('/api/customer', require('./api/customer.js'));
 
+// deployment
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
+
 // start server AFTER all routes/middleware registered
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
